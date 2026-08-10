eb0f2ba52982d48db35142db687d401585eacaab
amdadulhaquehasan <amdadul.hasan75757@gmail.com>
2026-08-10 15:35:46 +0600

    Create auth controller for user login

---
+++ SchoolManagementSystem.Api/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolManagementSystem.Business.Interfaces;
+using SchoolManagementSystem.Presentation.DTOs.Auth;
+
+namespace SchoolManagementSystem.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+
+    [HttpPost("login")]
+    [ProducesResponseType(typeof(AuthResponseDto), StatusCodes.Status200OK)]
+    public async Task<ActionResult<AuthResponseDto>> Login([FromBody] LoginDto dto)
+    {
+        var result = await _authService.LoginAsync(dto);
+        return Ok(result);
+    }
+}
