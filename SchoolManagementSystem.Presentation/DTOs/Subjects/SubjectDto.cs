c0f1a1fd5fcfa5eb0b09a1ddec9dff872b717788
amdadulhaquehasan <amdadul.hasan75757@gmail.com>
2026-08-14 16:29:07 +0600

    Update dtos on teacher assigned in subject

--- SchoolManagementSystem.Presentation/DTOs/Subjects/SubjectDto.cs
+++ SchoolManagementSystem.Presentation/DTOs/Subjects/SubjectDto.cs
@@ -1,5 +1,11 @@
 ﻿namespace SchoolManagementSystem.Presentation.DTOs.Subjects;
 
+public class AssignedTeacherDto
+{
+    public string Id { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+}
+
 public class SubjectDto
 {
     public int Id { get; set; }
@@ -7,5 +13,6 @@ public class SubjectDto
     public string? Description { get; set; }
     public int ClassCourseId { get; set; }
     public string ClassCourseName { get; set; } = string.Empty;
-    public List<string> AssignedTeacherNames { get; set; } = new();
+
+    public List<AssignedTeacherDto> AssignedTeachers { get; set; } = new();
 }
